9b6af75435c6ae24960449000ef5f57aeae9ac0c
mekahay <mekenziehayford@gmail.com>
2021-02-08 20:34:43 -0700

    heroku deploy

--- views/Contact.jsx
+++ views/Contact.jsx
@@ -6,8 +6,10 @@ class Contact extends React.Component {
         return(
             <Layout>
                 <body>
-                <a href="mailto:mekenzie.hayford@gmail.com">Email Us</a>
+                <a href="mailto:mekenzie.hayford@gmail.com">Email Me Mekenzie.hayford@gmail.com</a>
                 <p>cell: (253) 377-2501</p>
+                <a href="https://github.com/mekahay">GitHub</a>
+                <a href="https://www.linkedin.com/in/mekenziehayford/">LinkedIn</a>
                 </body>
             </Layout>
         )
